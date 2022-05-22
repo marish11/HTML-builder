cf6b84095080c6cc80a5d2dc1ea5a750605c025f
marish11 <marishokola89@gmail.com>
2022-05-22 22:25:27 +0300

    complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+  fs.readdir(path.join(__dirname, 'files-copy'), { withFileTypes: true }, (err, files) => {
+    if (err) {
+      throw err;
+    } else {
+      files.forEach(file => {
+        if (file.isFile()) {
+          fs.unlink(path.join(__dirname, 'files-copy', file.name), (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+    }
+  });
+  fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
+    if (err) {
+      throw err;
+    } else {
+      files.forEach(file => {
+        if (file.isFile()) {
+          fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+    }
+  });
+}
+
+copyDir();
